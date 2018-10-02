a4c4773a638563ec64d1d66461777b94a80bbd38
Nidhyabenny <nidhyabenny2020it.ajce.in>
2018-10-02 18:25:45 +0530

    database connection

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ var multer=require('multer');
 var upload=multer({dest: './uploads'});
 
 
+var User = require('../models/user');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -16,9 +19,11 @@ router.get('/', function(req, res, next) {
 
 function login(req,res){
 	try{
-		var username = req.body.username;
+		var email = req.body.email;
 		var password = req.body.password;
 		console.log(req.body);
+
+
 		res.send({
 			status:true,
 			msg : "login success"
@@ -35,5 +40,38 @@ function login(req,res){
 	}
 }
 router.post('/login',login);
+
+
+function register(req,res){
+	try{
+		var username = req.body.username;
+		var email = req.body.email;
+		var password = req.body.password;
+		console.log(req.body);
+		var newUser = new User({
+			username: username,
+			email: email,
+			password: password,
+		});
+		User.createUser(newUser, function(err,user){
+			if(err){ 
+				throw(err);
+			}
+			console.log(user);
+		}); 
+
+		
+		res.redirect('/');
+}
+		
+	catch(e){
+		res.send({
+			status:false,
+			msg : "registeration failed"
+		});
+
+	}
+}
+router.post('/register',register);
 
 module.exports = router;
