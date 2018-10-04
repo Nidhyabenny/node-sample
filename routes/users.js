f27b425bb8b410a932ef4130001da86de5039a5e
Nidhyabenny <nidhyabenny2020it.ajce.in>
2018-10-04 18:15:13 +0530

    fourth commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var multer=require('multer');
-var upload=multer({dest: './uploads'});
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 
 var User = require('../models/user');
@@ -21,13 +22,24 @@ function login(req,res){
 	try{
 		var email = req.body.email;
 		var password = req.body.password;
+		
 		console.log(req.body);
-
-
-		res.send({
-			status:true,
-			msg : "login success"
+		var newUser = new User({
+			
+			email: email,
+			password: password,
 		});
+		User.createUser(newUser, function(err,user){
+			if(err){ 
+				throw(err);
+			}
+			console.log(user);
+		}); 
+
+		
+		res.redirect('/');
+
+		
 
 
 	}
@@ -73,5 +85,44 @@ function register(req,res){
 	}
 }
 router.post('/register',register);
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getUserByUsername(username, function(err, user){
+    	if(err) throw err;
+    	if(!user){
+    		return done(null, false, {message: 'Unknown User'});
+    	}
+
+    	User.comparePassword(password, user.password, function(err, isMatch){
+    		if(err) throw err;
+    		if(isMatch){
+    			return done(null, user);
+    		}
+    		else{
+    			return done(null, false, {message: 'Invalid password'});
+    		}
+    	});
+    });
+  }));
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+
+
+router.post('/login',
+  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 module.exports = router;
